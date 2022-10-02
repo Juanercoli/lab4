2c117b5b1bbdd2be2622cd5464beb70718b21a84
Juan Ercoli <ercolijuan@yahoo.com.ar>
2022-10-02 04:23:02 +0000

    Agregada accion show para el controlador de Tweets.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,8 @@ class TweetsController < ApplicationController
     def index
         @tweets = Tweet.all.order(created_at: :desc)
     end
+    
+    def show   
+        @tweet = Tweet.find(params[:id])
+    end
 end
